44f82cd62aa8f85d67b0a4b00c308c56b9cdaeed
myusufbudiawan <60254108+myusufbudiawan@users.noreply.github.com>
2022-05-20 11:15:05 +0800

    streamlit

--- ExcelWeb.py
+++ ExcelWeb.py
@@ -28,3 +28,21 @@ st.write('You selected:', option)
 
 if option == 'CCC4':
     st.write(df2.iloc[0:7, :].astype(str))
+
+elif option == 'CCC2':
+    st.write(df2.iloc[9:15, :].astype(str))
+
+elif option == 'CCC6':
+    st.write(df2.iloc[17:18, :].astype(str))
+
+elif option == 'APCC':
+    st.write(df2.iloc[25:31, :].astype(str))
+
+elif option == 'ICC':
+    st.write(df2.iloc[33:39, :].astype(str))
+
+elif option == 'EMFP':
+    st.write(df2.iloc[41:42, :].astype(str))
+
+elif option == 'BRH1':
+    st.write(df2.iloc[49:56, :].astype(str))

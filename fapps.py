37645a7c603b302bc2f571e6146657b16dc80288
myusufbudiawan <60254108+myusufbudiawan@users.noreply.github.com>
2022-06-07 11:07:02 +0800

    fapps

--- fapps.py
+++ fapps.py
@@ -7,7 +7,8 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 
-st.set_page_config(page_title = 'Dell Factory Consolidate View',page_icon='Dell_Logo_Blue_rgb.png',layout="wide"  )
+st.set_page_config(page_title='Dell Factory Consolidate View',
+                   page_icon='Dell_Logo_Blue_rgb.png', layout="wide")
 
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
@@ -31,16 +32,17 @@ st.markdown("""
 st.header('Consolidate View')
 
 group = option_menu(
-        menu_title= "Factory",
-        options = ['ALL','BRH1','EMFP','CCC4','APCC','CCC2','CCC6','ICC'],
-        menu_icon = "building",
-        icons = ['bank2','bank2','bank2','bank2','bank2','bank2','bank2','bank2',],
-        styles={
-            "menu_icon": {"color":"#3498DB"},
-           "nav-link-selected": {"background-color": "#3498DB"} 
-        },
-        orientation="horizontal"
-    )
+    menu_title="Factory",
+    options=['ALL', 'BRH1', 'EMFP', 'CCC4', 'APCC', 'CCC2', 'CCC6', 'ICC'],
+    menu_icon="building",
+    icons=['bank2', 'bank2', 'bank2', 'bank2',
+           'bank2', 'bank2', 'bank2', 'bank2', ],
+    styles={
+        "menu_icon": {"color": "#3498DB"},
+        "nav-link-selected": {"background-color": "#3498DB"}
+    },
+    orientation="horizontal"
+)
 
 hide_st_style = """ 
                 <style>
@@ -50,79 +52,78 @@ hide_st_style = """
                 </style>
                 """
 
-st.markdown (hide_st_style, unsafe_allow_html=True)
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
-##-- LOAD DATAFRAME
-excel_file = r'C:\Users\Kamarul_Syamil\Desktop\Dell\Project\Consolidated Factory Workplan.xlsx'
+# -- LOAD DATAFRAME
+excel_file = r'Consolidated Factory Workplan.xlsx'
 sheet_name = 'Workplans'
 
 df = pd.read_excel(excel_file,
-                    sheet_name=sheet_name,
-                    usecols='B:K',
-                    skiprows= (0,1,2,3,4,5),
-                    header = None)
+                   sheet_name=sheet_name,
+                   usecols='B:K',
+                   skiprows=(0, 1, 2, 3, 4, 5),
+                   header=None)
 
 
 df1 = df.fillna('')
 time = pd.read_excel(excel_file,
-                    sheet_name=sheet_name,
-                    usecols='F:F',
-                    nrows=1,
-                    header=3)
+                     sheet_name=sheet_name,
+                     usecols='F:F',
+                     nrows=1,
+                     header=3)
 
 time1 = time.iloc[0][0]
 
 st.subheader('Update On : ' + time1)
 
-st.write('You have selected',group)
+st.write('You have selected', group)
 
 ccc4 = df1.iloc[0:9, :].astype(str)
 ccc2 = df1.iloc[9:18, :].astype(str)
 ccc6 = df1.iloc[17:21, :].astype(str)
 apcc = df1.iloc[25:33, :].astype(str)
 emfp = df1.iloc[41:44, :].astype(str)
-brh1 = df1.iloc[49:58, :].astype(str) 
+brh1 = df1.iloc[49:58, :].astype(str)
 icc = df1.iloc[33:41, :].astype(str)
 
 col1, col2, col3 = st.columns(3)
-cv,cn,cm = st.columns ((1,4,1))
-c1,c2,c3 = st.columns((2,2,2))
-c5,c6,c7 = st.columns((2,2,2))
-c8,c4,c9 = st.columns((2,2,2))
-cc,cb,ck = st.columns((2,2,2))
+cv, cn, cm = st.columns((1, 4, 1))
+c1, c2, c3 = st.columns((2, 2, 2))
+c5, c6, c7 = st.columns((2, 2, 2))
+c8, c4, c9 = st.columns((2, 2, 2))
+cc, cb, ck = st.columns((2, 2, 2))
 
 if group == 'ALL':
-    
 
     with c1:
         D2 = pd.read_excel(excel_file,
-                sheet_name=sheet_name,
-                usecols='J:J',
-                nrows=1,
-                header=14)
+                           sheet_name=sheet_name,
+                           usecols='J:J',
+                           nrows=1,
+                           header=14)
         t2 = pd.read_excel(excel_file,
-                sheet_name=sheet_name,
-                usecols='D:D',
-                nrows=1,
-                header=14)
+                           sheet_name=sheet_name,
+                           usecols='D:D',
+                           nrows=1,
+                           header=14)
 
         DD2 = D2.iloc[0][0]
         tt2 = t2.iloc[0][0]
         st.write('Date: ' + tt2)
-        st.write('Date: ' + DD2)  
+        st.write('Date: ' + DD2)
         st.write(ccc2)
-        
+
     with c2:
         D3 = pd.read_excel(excel_file,
-                sheet_name=sheet_name,
-                usecols='J:J',
-                nrows=1,
-                header=5)
+                           sheet_name=sheet_name,
+                           usecols='J:J',
+                           nrows=1,
+                           header=5)
         t3 = pd.read_excel(excel_file,
-                sheet_name=sheet_name,
-                usecols='D:D',
-                nrows=1,
-                header=5)
+                           sheet_name=sheet_name,
+                           usecols='D:D',
+                           nrows=1,
+                           header=5)
 
         DD3 = D3.iloc[0][0]
         tt3 = t3.iloc[0][0]
@@ -132,15 +133,15 @@ if group == 'ALL':
 
     with c3:
         D4 = pd.read_excel(excel_file,
-                sheet_name=sheet_name,
-                usecols='J:J',
-                nrows=1,
-                header=22)
+                           sheet_name=sheet_name,
+                           usecols='J:J',
+                           nrows=1,
+                           header=22)
         t4 = pd.read_excel(excel_file,
-                sheet_name=sheet_name,
-                usecols='D:D',
-                nrows=1,
-                header=22)
+                           sheet_name=sheet_name,
+                           usecols='D:D',
+                           nrows=1,
+                           header=22)
 
         DD4 = D4.iloc[0][0]
         tt4 = t4.iloc[0][0]
@@ -150,15 +151,15 @@ if group == 'ALL':
 
     with c5:
         D5 = pd.read_excel(excel_file,
-                sheet_name=sheet_name,
-                usecols='J:J',
-                nrows=1,
-                header=30)
+                           sheet_name=sheet_name,
+                           usecols='J:J',
+                           nrows=1,
+                           header=30)
         t5 = pd.read_excel(excel_file,
-                sheet_name=sheet_name,
-                usecols='D:D',
-                nrows=1,
-                header=30)
+                           sheet_name=sheet_name,
+                           usecols='D:D',
+                           nrows=1,
+                           header=30)
 
         DD5 = D5.iloc[0][0]
         tt5 = t5.iloc[0][0]
@@ -168,15 +169,15 @@ if group == 'ALL':
 
     with c6:
         D7 = pd.read_excel(excel_file,
-                sheet_name=sheet_name,
-                usecols='J:J',
-                nrows=1,
-                header=46)
+                           sheet_name=sheet_name,
+                           usecols='J:J',
+                           nrows=1,
+                           header=46)
         t7 = pd.read_excel(excel_file,
-                sheet_name=sheet_name,
-                usecols='D:D',
-                nrows=1,
-                header=46)
+                           sheet_name=sheet_name,
+                           usecols='D:D',
+                           nrows=1,
+                           header=46)
 
         DD7 = D7.iloc[0][0]
         tt7 = t7.iloc[0][0]
@@ -186,15 +187,15 @@ if group == 'ALL':
 
     with c7:
         D6 = pd.read_excel(excel_file,
-                sheet_name=sheet_name,
-                usecols='J:J',
-                nrows=1,
-                header=38)
+                           sheet_name=sheet_name,
+                           usecols='J:J',
+                           nrows=1,
+                           header=38)
         t6 = pd.read_excel(excel_file,
-                sheet_name=sheet_name,
-                usecols='D:D',
-                nrows=1,
-                header=38)
+                           sheet_name=sheet_name,
+                           usecols='D:D',
+                           nrows=1,
+                           header=38)
 
         DD6 = D6.iloc[0][0]
         tt6 = t6.iloc[0][0]
@@ -204,15 +205,15 @@ if group == 'ALL':
 
     with c4:
         D1 = pd.read_excel(excel_file,
-                sheet_name=sheet_name,
-                usecols='J:J',
-                nrows=1,
-                header=54)
+                           sheet_name=sheet_name,
+                           usecols='J:J',
+                           nrows=1,
+                           header=54)
         t1 = pd.read_excel(excel_file,
-                sheet_name=sheet_name,
-                usecols='D:D',
-                nrows=1,
-                header=54)
+                           sheet_name=sheet_name,
+                           usecols='D:D',
+                           nrows=1,
+                           header=54)
 
         DD1 = D1.iloc[0][0]
         tt1 = t1.iloc[0][0]
@@ -223,10 +224,10 @@ if group == 'ALL':
 if 'BRH1' in group:
     with cn:
         D1 = pd.read_excel(excel_file,
-                    sheet_name=sheet_name,
-                    usecols='J:J',
-                    nrows=1,
-                    header=54)
+                           sheet_name=sheet_name,
+                           usecols='J:J',
+                           nrows=1,
+                           header=54)
 
         DD1 = D1.iloc[0][0]
         st.write('Date: ' + DD1)
@@ -235,22 +236,22 @@ if 'BRH1' in group:
 if 'CCC2' in group:
     with cn:
         D2 = pd.read_excel(excel_file,
-                    sheet_name=sheet_name,
-                    usecols='J:J',
-                    nrows=1,
-                    header=14)
+                           sheet_name=sheet_name,
+                           usecols='J:J',
+                           nrows=1,
+                           header=14)
 
         DD2 = D2.iloc[0][0]
-        st.write('Date: ' + DD2)  
+        st.write('Date: ' + DD2)
         st.write(ccc2)
 
 if 'CCC4' in group:
     with cn:
         D3 = pd.read_excel(excel_file,
-                    sheet_name=sheet_name,
-                    usecols='J:J',
-                    nrows=1,
-                    header=5)
+                           sheet_name=sheet_name,
+                           usecols='J:J',
+                           nrows=1,
+                           header=5)
 
         DD3 = D3.iloc[0][0]
         st.write('Date: ' + DD3)
@@ -259,10 +260,10 @@ if 'CCC4' in group:
 if 'CCC6' in group:
     with cn:
         D4 = pd.read_excel(excel_file,
-                    sheet_name=sheet_name,
-                    usecols='J:J',
-                    nrows=1,
-                    header=22)
+                           sheet_name=sheet_name,
+                           usecols='J:J',
+                           nrows=1,
+                           header=22)
 
         DD4 = D4.iloc[0][0]
         st.write('Date: ' + DD4)
@@ -271,10 +272,10 @@ if 'CCC6' in group:
 if 'APCC' in group:
     with cn:
         D5 = pd.read_excel(excel_file,
-                    sheet_name=sheet_name,
-                    usecols='J:J',
-                    nrows=1,
-                    header=30)
+                           sheet_name=sheet_name,
+                           usecols='J:J',
+                           nrows=1,
+                           header=30)
 
         DD5 = D5.iloc[0][0]
         st.write('Date: ' + DD5)
@@ -283,10 +284,10 @@ if 'APCC' in group:
 if 'ICC' in group:
     with cn:
         D6 = pd.read_excel(excel_file,
-                    sheet_name=sheet_name,
-                    usecols='J:J',
-                    nrows=1,
-                    header=38)
+                           sheet_name=sheet_name,
+                           usecols='J:J',
+                           nrows=1,
+                           header=38)
 
         DD6 = D6.iloc[0][0]
         st.write('Date: ' + DD6)
@@ -295,21 +296,22 @@ if 'ICC' in group:
 if 'EMFP' in group:
     with cn:
         D7 = pd.read_excel(excel_file,
-                    sheet_name=sheet_name,
-                    usecols='J:J',
-                    nrows=1,
-                    header=46)
+                           sheet_name=sheet_name,
+                           usecols='J:J',
+                           nrows=1,
+                           header=46)
 
         DD7 = D7.iloc[0][0]
         st.write('Date: ' + DD7)
         st.write(emfp)
 
 with cb:
-    st.write('To download the full page of consolidate view, click download button below :')
+    st.write(
+        'To download the full page of consolidate view, click download button below :')
     btn = st.download_button(
-    label= 'Download File',
-    data= excel_file,
-    file_name= excel_file,
+        label='Download File',
+        data=excel_file,
+        file_name=excel_file,
     )
 
 st.markdown("""
